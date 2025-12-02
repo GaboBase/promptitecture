316c7e5fc90d2cf461129978e7d97cbc247ed63e
Gabit-hub <168240954+GaboBase@users.noreply.github.com>
2025-12-02 00:53:49 -0300

    feat: Create src/javascript interface layer structure
    - Initialize src/javascript/interface/api directory
    - Add index.js with module documentation
    - Establish JavaScript implementation structure per Screaming Architecture

---
+++ src/javascript/interface/api/index.js
@@ -0,0 +1,13 @@
+/**
+ * API Interface Layer
+ * 
+ * This module contains REST API controllers and endpoints.
+ * It adapts external HTTP requests to internal use cases.
+ * 
+ * @module interface/api
+ * @version 0.1.0
+ */
+
+module.exports = {
+  // API controllers will be exported here
+};
